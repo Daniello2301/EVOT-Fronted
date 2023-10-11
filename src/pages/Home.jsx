f612310baea7ece9d6624bbc95daf935fcc6dda5
jaimeandres27 <41581346+jaimeandres27@users.noreply.github.com>
2023-10-10 22:41:33 -0500

    cambios ortografia

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         <div className="grid justify-center h-full max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-blue_dark dark:text-blue_dark">
-              Vaida tus titulos
+              Valida tus titulos
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             Utiliza tu documento de identificación para explorar de manera sencilla todos los cursos, diplomas y programas que has completado en colaboración con nuestros socios
