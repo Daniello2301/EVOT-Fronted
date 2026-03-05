4ab2eda80c415ad007a6c8f4ded9cf5acddc67a7
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2026-03-05 18:04:39 -0500

    Refactor authentication services to improve structure and clarity, including login, logout, refresh token, and reset password functionalities.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,33 @@
 import { axiosConfig } from "../helpers/axios.config";
 
-export const login = async(data) => {
-    const response = await axiosConfig.post(`auth`, data);
-    return response;
-} 
+// AuthService.js
+export const loginService = async (data) => {
+    const response = await axiosConfig.post('auth/login', data);
+    return response.data;
+};
+
+// Logout service to invalidate the token on the server
+export const logoutService = async (token) => {
+    const response = await axiosConfig.post('auth/logout', {}, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    return response.data;
+};
+
+// Refresh token service to get a new access token using the refresh token
+export const refreshTokenService = async (refreshToken) => {
+    // Send the refresh token in the Authorization header
+    const response = await axiosConfig.post('auth/refresh', {}, {
+        headers: { Authorization: `Bearer ${refreshToken}` }
+    });
+    return response.data;
+};
+
+// Reset password service to update the user's password
+export const resetPasswordService = async (data, token) => {
+    // Send the new password data along with the access token in the Authorization header
+    const response = await axiosConfig.put('auth/reset-password', data, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    return response.data;
+};
