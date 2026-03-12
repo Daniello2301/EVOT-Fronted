2f6178a3b4e0ca16ca1ea6c1affc75f8b9c4ae2a
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2026-03-12 12:09:53 -0500

    Refactor institutions service to return full response objects instead of only data

--- src/services/institucions.service.js
+++ src/services/institucions.service.js
@@ -3,47 +3,47 @@ import { axiosConfig } from '../helpers/axios.config';
 // Público
 export const getActivePartners = async () => {
     const res = await axiosConfig.get('institutions/actives');
-    return res.data;
+    return res;
 };
 
 // Admin
 export const getAllInstituciones = async (params = {}) => {
     const res = await axiosConfig.get('institutions', { params });
-    return res.data;
+    return res;
 };
 
 export const getInstitucionById = async (id) => {
     const res = await axiosConfig.get(`institutions/${id}`);
-    return res.data;
+    return res;
 };
 
 export const createInstitucion = async (data) => {
     const res = await axiosConfig.post('institutions', data);
-    return res.data;
+    return res;
 };
 
 export const updateInstitucion = async (id, data) => {
     const res = await axiosConfig.put(`institutions/${id}`, data);
-    return res.data;
+    return res;
 };
 
 export const deactivateInstitucion = async (id) => {
     const res = await axiosConfig.patch(`institutions/${id}/deactivate`);
-    return res.data;
+    return res;
 };
 
 export const activateInstitucion = async (id) => {
     const res = await axiosConfig.patch(`institutions/${id}/activate`);
-    return res.data;
+    return res;
 };
 
 // Institución
 export const getMyInstitucion = async () => {
     const res = await axiosConfig.get('institutions/me');
-    return res.data;
+    return res;
 };
 
 export const updateMyInstitucion = async (data) => {
     const res = await axiosConfig.put('institutions/me', data);
-    return res.data;
+    return res;
 };
