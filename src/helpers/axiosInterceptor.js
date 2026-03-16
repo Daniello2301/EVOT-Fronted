9e04086184b75c9ad68136435dd594951d795ee6
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2026-03-16 18:02:22 -0500

    Refactor token refresh logic to handle errors and improve token renewal process

--- src/helpers/axiosInterceptor.js
+++ src/helpers/axiosInterceptor.js
@@ -63,22 +63,26 @@ export const setupInterceptors = ({
             isRefreshing = true;
 
             try {
-
                 const newToken = await refreshToken();
 
-                isRefreshing = false;
+                
+                if (!newToken) {
+                    isRefreshing = false;
+                    refreshSubscribers = [];
+                    logout();
+                    return Promise.reject(new Error('No se pudo renovar el token'));
+                }
 
+                isRefreshing = false;
                 onRefreshed(newToken);
 
                 originalRequest.headers.Authorization = `Bearer ${newToken}`;
-
                 return axiosConfig(originalRequest);
 
             } catch (err) {
-
                 isRefreshing = false;
+                refreshSubscribers = []; 
                 logout();
-
                 return Promise.reject(err);
             }
         }
