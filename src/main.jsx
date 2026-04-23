380fa580ef869847c9177ee6acdb62d817082b52
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2026-04-23 16:35:36 -0500

    feat: add SidebarContext for managing sidebar state and behavior

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { AppWrapper } from "./components/common/PageMeta.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <AppWrapper>
+          <App />
+        </AppWrapper>
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
